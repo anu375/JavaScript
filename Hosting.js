329da4d9177540dfdcb22e591a076de8c49d3ce6
anu <anujadhav32004@gmail.com>
2026-01-25 19:14:47 +0530

    Hosting :::

---
+++ Hosting.js
@@ -0,0 +1,10 @@
+console.log(a); //undefined
+var a = 10;
+
+
+console.log(b); // Cannot access 'b' before initialization
+let b = 20;
+
+
+console.log(c); // Cannot access 'c' before initialization
+const c = 20;
