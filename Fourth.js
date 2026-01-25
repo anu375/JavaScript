7687c970916a6188364f40fbafaad6c5748994ad
anu <anujadhav32004@gmail.com>
2026-01-25 19:05:42 +0530

    JavaScript :::

---
+++ Fourth.js
@@ -0,0 +1,63 @@
+let str = "    Welcome to Nashik    ";
+
+console.log("UpperCase : " + str.toUpperCase());
+
+console.log("Length : " + str.length);
+
+console.log("LowerCase : " + str.toLowerCase());
+
+console.log("Trim : " + str.trim());
+
+console.log("TrimStart : " + str.trimStart());
+
+console.log("TrimEnd : " + str.trimEnd());
+
+console.log("CharAt : " + str.charAt(7));
+
+console.log("charCodeAt : " + str.charCodeAt(7));
+
+console.log("indexOf : " + str.indexOf("to"));
+
+console.log("LastIndexOf : " + str.lastIndexOf("k"));
+
+console.log("Includes : " + str.includes("Nashik"));
+
+console.log("StartsWith : " + str.startsWith("    W"));
+
+console.log("EndsWith : " + str.endsWith("k    "));
+
+console.log("Substring : " + str.substring(2, 11));
+
+console.log("Substr : " + str.substr(2, 6));
+
+console.log("Replace : " + str.replace("Nashik", "Banglore"));
+
+console.log("ReplaceAll : " + str.replaceAll("a", "A"));
+
+console.log("Split : " + str.split(""));
+
+console.log("Concat : " + str.concat("   - Maharashtra"));
+
+console.log("Repeat : " + "jsS".repeat(3));
+
+console.log("Repeat : " + str.repeat(3));
+
+console.log("PadStart : " + str.padStart(3, "0"));
+
+console.log("PadEnd : " + str.padEnd(3, "0"));
+
+console.log("Match : " + str.match(/a/g));
+
+console.log("Search : " + str.search("com"));
+
+console.log("ValueOf : " + str.valueOf());
+
+console.log("ToString : " + str.toString());
+
+console.log("a".localeCompare("b"));
+
+console.log("At : " + str.charAt(8));
+
+console.log("At : " + str.charAt(-1));
+
+console.log("e".normalize());
