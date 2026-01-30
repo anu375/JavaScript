d64dbaeb0ec2b57b0c6e562ad0bca6a6cd72f69d
anu <anujadhav32004@gmail.com>
2026-01-30 12:14:58 +0530

    Closure ::

---
+++ Closure/Demo.js
@@ -0,0 +1,19 @@
+function outer() {
+
+    let count = 0;
+
+    function inner() {
+        count++;
+        console.log(count);
+    }
+
+    return inner;
+}
+
+const fn = outer();
+
+fn();
+
+fn();
+
+fn();
